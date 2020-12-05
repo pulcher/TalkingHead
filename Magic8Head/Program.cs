e894e92d65bcd2909ab2c6cc213c49884badc604
Harold Pulcher <pulcher@killercomputing.com>
2020-12-05 03:46:24 +0000

    Start of the Magic8Hea

---
+++ Magic8Head/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Device.Gpio.Drivers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Magic8Head
+{
+    class Program
+    {
+        static int buttonPin = 7;
+        static GpioController controller;
+
+        static async Task Main(string[] args)
+        {
+            SetupGPIO();
+
+            while (true)
+            {
+                var status = controller.Read(buttonPin);
+                Console.WriteLine ($"Button is {status}");
+                Thread.Sleep (100);
+            }
+        }
+
+        public static void SetupGPIO()
+        {
+            System.Console.WriteLine("Setiing up GPIO...");
+            controller = new GpioController(0, new RaspberryPi3Driver());
+
+            controller.OpenPin (buttonPin, PinMode.InputPullUp);
+        }
+    }
+}
