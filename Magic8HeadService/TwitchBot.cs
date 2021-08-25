f32c972285b3bc9b89699d101b7afc66ee6bc86f
Harold Pulcher <pulcher@killercomputing.com>
2021-08-25 04:59:36 +0100

    added twitchbot class, and start of the configuration class

---
+++ Magic8HeadService/TwitchBot.cs
@@ -0,0 +1,21 @@
+using System;
+using TwitchLib.Client;
+using TwitchLib.Client.Enums;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Extensions;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace Magic8HeadService.TwitchBot
+{
+    public class TwitchBot 
+    {
+        TwitchClient client;
+
+        public TwitchBot()
+        {
+            
+        }
+    }
+}
---
+++ Magic8HeadService/TwitchBotConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Magic8HeadService.TwitchBotConfiguration
+{
+    public class TwitchBotConfiguration
+    {
+        public string UserName { get; set; }
+        public string AccessTocket { get; set; }
+    }
+}
