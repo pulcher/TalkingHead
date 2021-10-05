a34cf8fec53e3acba82e569fa407fcbc024d3489
Harold Pulcher <pulcher@pulcher.biz>
2021-10-04 22:45:28 -0500

    stoooopid scopled stuff

--- Magic8HeadService/Program.cs
+++ Magic8HeadService/Program.cs
@@ -2,15 +2,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MrBigHead.Services;
+using System.Threading.Tasks;
 
 namespace Magic8HeadService
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            host.Run();
+            await host.StartAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
