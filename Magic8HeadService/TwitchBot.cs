a2de182b85b7ddfe25b822356ea7f24b87491429
Harold Pulcher <pulcher@killercomputing.com>
2022-03-17 02:24:01 +0000

    Got the uptime command listened to

--- Magic8HeadService/TwitchBot.cs
+++ Magic8HeadService/TwitchBot.cs
@@ -125,29 +125,39 @@ namespace Magic8HeadService
             logger.LogInformation($"command is: {e.Command.CommandText.ToLower()}");
             logger.LogInformation($"args is null? : '{e.Command.ArgumentsAsList == null}'");
 
-            switch (e.Command.ArgumentsAsList.FirstOrDefault()?.ToLower())
+            switch (e.Command.CommandText.ToLower())
             {
-                case AvailableCommands.Help:
-                // case null:
-                    action = new HelpCommand(client, logger);
-                    break;
-                case AvailableCommands.Ask:
-                	action = new AskCommand(client, sayingResponse, mood, logger);
-                	break;
-                case AvailableCommands.Dad:
-                    action = new DadCommand(client, sayingResponse, dadJokeService, logger);
-                    break;
-                case AvailableCommands.Inspire:
-                    action = new InspirationalCommand(client, sayingResponse, logger);
-                    break;
-                case AvailableCommands.Say:
-                    action = new SayCommand(client, sayingResponse, logger);
-                    break;
+                case "uptime":
+                    logger.LogInformation($"Gotta report some uptime...");
+                    return;
+                case "mbh":
+                    switch (e.Command.ArgumentsAsList.FirstOrDefault()?.ToLower())
+                        {
+                            case AvailableCommands.Help:
+                            // case null:
+                                action = new HelpCommand(client, logger);
+                                break;
+                            case AvailableCommands.Ask:
+                                action = new AskCommand(client, sayingResponse, mood, logger);
+                                break;
+                            case AvailableCommands.Dad:
+                                action = new DadCommand(client, sayingResponse, dadJokeService, logger);
+                                break;
+                            case AvailableCommands.Inspire:
+                                action = new InspirationalCommand(client, sayingResponse, logger);
+                                break;
+                            case AvailableCommands.Say:
+                                action = new SayCommand(client, sayingResponse, logger);
+                                break;
+                            default:
+                                break;
+                        }
+                        
+                        action.Handle(e);
+                    return;
                 default:
-                    break;
+                    return;
             }
-
-            action.Handle(e);
 		}
     }
 }
