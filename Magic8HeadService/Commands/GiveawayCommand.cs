e86097f5ac2cf5dc35f5c8f498621e266ac992d5
Harold Pulcher <pulcher@killercomputing.com>
2022-04-08 19:29:17 +0000

    added giveaway command

---
+++ Magic8HeadService/Commands/GiveawayCommand.cs
@@ -0,0 +1,28 @@
+
+using Magic8HeadService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+
+public class GiveawayCommand : ICommandMbhToTwitch
+{
+    private readonly TwitchClient client;
+    private readonly IConfiguration config;
+    private readonly ILogger<Worker> logger;
+
+    public string Name => "giveaway";
+
+    public GiveawayCommand(TwitchClient client, IConfiguration config, ILogger<Worker> logger)
+    {
+        this.client = client;
+        this.config = config;
+        this.logger = logger;
+    }
+
+    public void Handle(OnChatCommandReceivedArgs args)
+    {
+        client.SendMessage(args.Command.ChatMessage.Channel, 
+            "Win a $100 Amazon Gift Card.  Register at https://contests.davidriewe.com/harold-pulcher-give-away-4-22/");
+    }
+}
