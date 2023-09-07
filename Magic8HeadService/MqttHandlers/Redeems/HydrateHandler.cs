561daf9c4cb17c7995157fa2773e10dcf0d84e6f
Harold Pulcher <pulcher@pulcher.biz>
2023-09-06 21:38:29 -0500

    added first real handler

---
+++ Magic8HeadService/MqttHandlers/Redeems/HydrateHandler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Linq;
+using TwitchLib.Client.Interfaces;
+using System.Text.Json;
+using System.Text;
+
+namespace Magic8HeadService.MqttHandlers.Redeems
+{
+    public class HydrateHandler : IMqttHandler
+    {
+        private ITwitchClient client;
+        private ILogger<Worker> logger;
+
+        public HydrateHandler(ITwitchClient client, ILogger<Worker> logger)
+        {
+            this.client = client;
+            this.logger = logger;
+        }
+
+        public bool CanHandle(MqttHandlerMessage message)
+        {
+            if (message == null) return false;
+
+            var payloadString = Encoding.ASCII.GetString(message.Payload);
+
+            var redeem = JsonSerializer.Deserialize<MqttRedeemPayload>(payloadString);
+
+            if (redeem != null && redeem.RewardName == "Hydrate!") 
+                return true;
+            else
+                return false;
+        }
+
+        public void Handle(MqttHandlerMessage message)
+        {
+            // Encoding.ASCII.GetString(message.Payload)
+            client.SendMessage(client.JoinedChannels.FirstOrDefault(), "!mbh say Yo time for some hydration!");
+        }
+    }
+}
