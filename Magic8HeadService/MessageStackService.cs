80d723dcd780e92defccb4485f62d5612b371d2e
Harold Pulcher <pulcher@pulcher.biz>
2023-11-06 22:07:44 -0600

    Added the message stack service

---
+++ Magic8HeadService/MessageStackService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using TwitchLib.Client.Models;
+
+namespace Magic8HeadService
+{
+    public class MessageStackService : IMessageStackService
+    {
+        private readonly Stack<ChatMessage> messageStack;
+
+        public MessageStackService()
+        {
+            messageStack = new Stack<ChatMessage>(30);
+        }
+        public ChatMessage GetNextMessage()
+        {
+            messageStack.TryPop(out var result);
+            return result;
+        }
+
+        public ChatMessage GetPreviousMessage(int previousIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PutMessage(ChatMessage message)
+        {
+            if (message == null) return;
+
+            messageStack.TrimExcess();
+            messageStack.Push(message);
+        }
+    }
+}
