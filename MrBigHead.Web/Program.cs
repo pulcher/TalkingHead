177116fcb811bc2f67fb75e0659c4121e3853994
Harold Pulcher <pulcher@pulcher.biz>
2024-01-15 23:29:35 -0600

    need to add in the user secrets for the client ID

--- MrBigHead.Web/Program.cs
+++ MrBigHead.Web/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using MrBigHead.Web.Services;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -14,7 +16,10 @@ namespace MrBigHead.Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddSingleton<UserInformationProvider, UserInformationProvider>();
 
             builder.Services.AddMsalAuthentication(options =>
             {
