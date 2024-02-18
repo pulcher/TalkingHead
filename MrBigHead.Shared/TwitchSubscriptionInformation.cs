d887ee82985b8b51f44d0d358ac1ee0f0c623122
Harold Pulcher <pulcher@pulcher.biz>
2024-02-18 10:08:07 -0600

    Added in the twitch sub info

---
+++ MrBigHead.Shared/TwitchSubscriptionInformation.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MrBigHead.Shared
+{
+    public class TwitchSubscriptionInformation
+    {
+        [JsonPropertyName("broadcaster_id ")]
+        public string BroadcasterId { get; set; }
+        [JsonPropertyName("broadcaster_login")]
+        public string BroadcasterLogin { get; set; }
+        [JsonPropertyName("broadcaster_name")]
+        public string BroadcasterName { get; set; }
+        [JsonPropertyName("gifter_id")]
+        public string GifterId { get; set; }
+        [JsonPropertyName("gifter_login")]
+        public string GifterLogin { get; set; }
+        [JsonPropertyName("gifter_name")]
+        public string GifterName { get; set; }
+        [JsonPropertyName("is_gift")]
+        public bool IsGift { get; set; }
+        [JsonPropertyName("tier")]
+        public string Tier { get; set; }
+    }
+}
